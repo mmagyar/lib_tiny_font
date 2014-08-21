d4a8c50f39e1da5867868e7a965c912c7cca3e8d
Magyar Máté <hunyeti@gmail.com>
2014-08-21 21:42:51 +0200

    initial commit

---
+++ src/bdf_parser/bdf_parser.c
@@ -0,0 +1,118 @@
+#include "bdf_parser.h"
+int read_file(char  * const file_name)
+{
+    FILE * f = fopen(file_name,"r");
+    int const READ_BUFFER_ELEMENTS = 1000;
+    // int const READ_BUFFER_SIZE = READ_BUFFER_ELEMENTS* READ_DATA_SIZE;
+    //char * const reader = malloc(READ_DATA_SIZE*READ_BUFFER_ELEMENTS);
+    char reader[READ_BUFFER_ELEMENTS];
+
+    while(1) {
+
+        if(fgets(reader,READ_BUFFER_ELEMENTS,f)==NULL) {
+            if(feof(f)!= 0) {
+                printf("\nEnd of file has been reached\n");
+            } else if(ferror(f)!=0) {
+                printf("\nAn error occured during  file read");
+            }
+            break;
+
+        }
+        if(!strncmp(reader,"STARTFONT ",10)) {
+            printf(reader);
+        } else if(!strncmp(reader,"COMMENT ",8)) {
+            printf(reader);
+        } else if(!strncmp(reader,"CONTENTVERSION",12)) {
+            printf(reader);
+        } else if(!strncmp(reader,"FONT ",5)) {
+            printf(reader);
+        } else if(!strncmp(reader,"SIZE ",5)) {
+            printf(reader);
+        }
+        else if(!strncmp(reader,"METRICSSET ",11)) {
+            printf(reader);
+        }
+
+        else if(!strncmp(reader,"METRICSSET ",11)) {
+            printf(reader);
+        }
+
+        else if(!strncmp(reader,"METRICSSET ",11)) {
+            printf(reader);
+        }
+
+
+ else         if(!strncmp(reader,"FONTBOUNDINGBOX",15)) {        }
+        else if(!strncmp(reader,"PIXEL_SIZE",10)) {}
+        else if(!strncmp(reader,"STARTCHAR",9)) {}
+        else if(!strncmp(reader,"ENCODING",8)) {}
+        else if(!strncmp(reader,"BBX",3)) {}
+        else if(!strncmp(reader,"BITMAP",6)) {}
+        else if(!strncmp(reader,"ENDCHAR",7)) {}
+        else {
+            //printf("n");
+        }
+        /*
+        switch (lineSplit[0]) {
+        case "FONTBOUNDINGBOX":
+            // Out.log("FONT: " + lineSplit[1]);
+            box = lineSplit[1].split(" ");
+            width = Integer.parseInt(box[0]);
+            height = Integer.parseInt(box[1]);
+            offX = Integer.parseInt(box[2]);
+            offY = Integer.parseInt(box[3]);
+            break;
+        case "PIXEL_SIZE":
+            size = Integer.parseInt(lineSplit[1]);
+            break;
+        case "STARTCHAR":
+            if (readingChar || readingCharBitmap)
+                Out.error("ERROR char started before previveous enden.");
+            readingChar = true;
+            // thisCharName = lineSplit[1];
+
+            break;
+        case "ENCODING":
+            thisCharCode = Integer.parseInt(lineSplit[1]);
+            break;
+        case "BBX":
+            box = lineSplit[1].split(" ");
+            cW = Integer.parseInt(box[0]);
+            cH = Integer.parseInt(box[1]);
+            cX = Integer.parseInt(box[2]);
+            cY = Integer.parseInt(box[3]);
+            break;
+        case "BITMAP":
+            if (readingChar)
+                readingCharBitmap = true;
+            else
+                Out.error("Error while Parsing, No STARTCHAR before BITMAP");
+            lineArray = new long[cH];
+            lineCounter = 0;
+            break;
+
+        case "ENDCHAR":
+            if (!(readingChar && readingCharBitmap))
+                Out.error("ERROR, char ended before it was started");
+            readingChar = false;
+            readingCharBitmap = false;
+            // Out.log("   " + thisCharName + ": " + (char) thisCharCode + " Size: " + cW + " : " + cH);
+
+            Font newFont = new Font(cW, cH, cX, cY, (char) thisCharCode, lineArray);
+            types.put(newFont.charachter, newFont);
+
+            // Out.log(String.format("0x%8s", Long.toHexString(lng)).replace(' ', '0'));
+
+            break;
+        default:
+            if (readingChar && readingCharBitmap)
+                lineArray[lineCounter++] = Long.parseLong(line, 16);
+            break
+        */
+        //        printf(reader);
+
+    }
+
+		fclose(f);
+    return 0;
+}
---
+++ src/bdf_parser/bdf_parser.h
@@ -0,0 +1,10 @@
+#ifndef BDF_PARSER_H
+#define BDF_PRASER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int read_file(char * const);
+
+#endif
+
